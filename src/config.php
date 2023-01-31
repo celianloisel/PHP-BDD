98c7cb88b4c057b19d0b16f2301e392bb166687b
celianloisel <celianloisel@gmail.com>
2023-01-31 16:07:43 +0100

    Db Manager Update  init

---
+++ src/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = [];
+$config['db'] = [
+    'name' => 'bank',
+    'host' => 'localhost',
+    'port' => 3306,
+    'user' => 'root',
+    'pass' => ''
+];
--- src/db.php
+++ src/db.php
@@ -1,1 +1,10 @@
 <?php
+
+require_once __DIR__ . "/config.php";
+
+try {
+    $dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'] . ';port=' . $config['db']['port'];
+    $db = new PDO($dsn, $config['db']['user'], $config['db']['pass']);
+} catch (PDOException $e) {
+    die('Erreur MySQL. ' . $e->getMessage());
+}
