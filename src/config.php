a38e73ba7bf537b5bde614c8dcd07e56d37b0ead
Coralie Pereira <coralie-p@outlook.fr>
2023-02-01 10:11:57 +0100

    role2

--- src/config.php
+++ src/config.php
@@ -17,17 +17,47 @@ define("ROLE_USER_UNVERIFIED", 1);
 define("ROLE_USER_VERIFIED", 10);
 define("ROLE_USER_BANNED", 0);
 
-// Vérification du rôle de l'utilisateur
+
+require_once "config.php";
+
 function checkRole($userRole, $requiredRole) {
   return $userRole >= $requiredRole;
 }
 
-// Exemple d'utilisation pour vérifier si l'utilisateur est un administrateur
-$userRole = ROLE_ADMIN;
+require_once "config.php";
+
+// Connexion à la base de données
+$host = "localhost";
+$dbname = "bank";
+$username = "root";
+$password = "root";
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+// Récupération du rôle de l'utilisateur à partir de la table `users`
+$stmt = $pdo->prepare("SELECT role FROM users WHERE username = :username");
+$stmt->execute(array(":username" => "bob"));
+$userRole = $stmt->fetchColumn();
+
+// Vérification du rôle de l'utilisateur
 if (checkRole($userRole, ROLE_ADMIN)) {
   // L'utilisateur a les autorisations d'un administrateur
+  echo "Bienvenue l'utilisateur";
 } else {
   // L'utilisateur n'a pas les autorisations d'un administrateur
+  echo "Votre accès estgits refusé";
 }
+
+// Vérification du rôle de l'utilisateur
+// function checkRole($userRole, $requiredRole) {
+//   return $userRole >= $requiredRole;
+// }
+
+// // Exemple d'utilisation pour vérifier si l'utilisateur est un administrateur
+// $userRole = ROLE_ADMIN;
+// if (checkRole($userRole, ROLE_ADMIN)) {
+//   // L'utilisateur a les autorisations d'un administrateur
+// } else {
+//   // L'utilisateur n'a pas les autorisations d'un administrateur
+// }
 
 ?>
