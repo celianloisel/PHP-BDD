dd31cceafbf172db044fb66a62415f0dc4fee8a2
Coralie Pereira <coralie-p@outlook.fr>
2023-02-01 09:59:10 +0100

    role

--- src/config.php
+++ src/config.php
@@ -8,3 +8,26 @@ $config['db'] = [
 	'user' => 'root',
 	'pass' => 'root'
 ];
+
+
+// Fichier de configuration pour les rôles
+define("ROLE_ADMIN", 1000);
+define("ROLE_MANAGER", 200);
+define("ROLE_USER_UNVERIFIED", 1);
+define("ROLE_USER_VERIFIED", 10);
+define("ROLE_USER_BANNED", 0);
+
+// Vérification du rôle de l'utilisateur
+function checkRole($userRole, $requiredRole) {
+  return $userRole >= $requiredRole;
+}
+
+// Exemple d'utilisation pour vérifier si l'utilisateur est un administrateur
+$userRole = ROLE_ADMIN;
+if (checkRole($userRole, ROLE_ADMIN)) {
+  // L'utilisateur a les autorisations d'un administrateur
+} else {
+  // L'utilisateur n'a pas les autorisations d'un administrateur
+}
+
+?>
