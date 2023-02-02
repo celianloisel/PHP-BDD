0ba91365caf7e8f2ef4f93ed3e3e82a22771ab90
Kisat0 <ghanembenyoucef@gmail.com>
2023-02-02 23:16:43 +0100

    fix wrong id name

--- src/assets/js/conversion.js
+++ src/assets/js/conversion.js
@@ -1,7 +1,7 @@
 console.log("script chargé !");
 console.log(document.cookie);
 
-var result = document.getElementById('amount-to');
+var result = document.getElementById('amount-convert-to');
 var amount = document.getElementById('amount').value;
 
 result.value = document.cookie * amount;
