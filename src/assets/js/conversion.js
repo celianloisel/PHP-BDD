fd3d56098eec08ddb06b3be334cae93cb733758c
Kisat0 <ghanembenyoucef@gmail.com>
2023-02-02 23:16:12 +0100

    add js for the live result of the conversion

--- src/assets/js/conversion.js
+++ src/assets/js/conversion.js
@@ -1,2 +1,8 @@
 console.log("script chargé !");
 console.log(document.cookie);
+
+var result = document.getElementById('amount-to');
+var amount = document.getElementById('amount').value;
+
+result.value = document.cookie * amount;
+console.log(result.value);
