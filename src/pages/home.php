e72df538f282d5862a7f86f44a85ec2528a0bec8
NoéHLM <noeheylemadec@gmail.com>
2023-02-03 09:36:28 +0100

    role c bon

--- src/pages/home.php
+++ src/pages/home.php
@@ -1,11 +1,11 @@
 <h1>Welcome on the home page !!</h1>
 
 <?php
-if ($role['status']>200) { echo "Bonjour ". $role['firstname'] ." OMGGGG C EST LE PATRONNNN BAISSEZ VOTRE PANTALON TOUTE DE SUITE";}
-elseif ($role['status']>10) { echo "Bonjour ". $role['firstname'] ." Oh Grand manager, Voulez-vous un café ? ";}
-elseif ($role['status']>1) {echo "Bonjour ". $role['firstname'] ." Votre compte est vérifié, vous avez accès à toutes les fonctionnalités aux utilisateurs de la banque"; }
-elseif ($role['status']>0)
+if ($admin>200) { echo "Bonjour ". $role['firstname'] ." OMGGGG C EST LE PATRONNNN BAISSEZ VOTRE PANTALON TOUTE DE SUITE";}
+elseif ($manager>10) { echo "Bonjour ". $role['firstname'] ." Oh Grand manager, Voulez-vous un café ? ";}
+elseif ($verified>1) {echo "Bonjour ". $role['firstname'] ." Votre compte est vérifié, vous avez accès à toutes les fonctionnalités aux utilisateurs de la banque"; }
+elseif ($unverified>0)
 echo "Bonjour ". $role['firstname'] ." Votre compte n'est pas vérifié. Veuillez attendre qu'un manager vous accepte.";
-elseif ($role['status']>(-1)) {
+elseif ($banned>(-1)) {
     echo "Bonjour ". $role['firstname'] ." T'es ban gros caca, ton odeur est fétide va prendre une douche et va en enfer.";
 }
