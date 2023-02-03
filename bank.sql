30d89e838ae231b603a432509637348ab0239203
celianloisel <celianloisel@gmail.com>
2023-02-03 13:20:17 +0100

    sql update

--- bank.sql
+++ bank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 02 fév. 2023 à 16:24
+-- Généré le : ven. 03 fév. 2023 à 12:19
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -33,20 +33,7 @@ CREATE TABLE IF NOT EXISTS `bankaccounts` (
   `user_id` int(11) NOT NULL,
   `value` varchar(255) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `bankaccounts`
---
-
-INSERT INTO `bankaccounts` (`id`, `user_id`, `value`) VALUES
-(1, 5, '[]'),
-(2, 6, '[]'),
-(4, 6, '[]'),
-(5, 6, '[]'),
-(6, 6, '[]'),
-(7, 8, '[]'),
-(8, 7, '[]');
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -61,16 +48,7 @@ CREATE TABLE IF NOT EXISTS `currencies` (
   `taux` float NOT NULL,
   `devise` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `currencies`
---
-
-INSERT INTO `currencies` (`id`, `name`, `taux`, `devise`) VALUES
-(1, 'Euro', 0.91, 'euro'),
-(2, 'Dollar', 1, 'dolUs'),
-(3, 'Dollar Canadien', 1.33, 'dolCan');
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -85,15 +63,7 @@ CREATE TABLE IF NOT EXISTS `deposits` (
   `value` float NOT NULL,
   `currencies_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `deposits`
---
-
-INSERT INTO `deposits` (`id`, `user_id`, `value`, `currencies_id`) VALUES
-(1, 6, 60, 1),
-(3, 6, 10, 2);
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -108,15 +78,9 @@ CREATE TABLE IF NOT EXISTS `transactions` (
   `value` float NOT NULL,
   `currencies_id` int(11) NOT NULL,
   `type` varchar(100) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `transactions`
---
-
-INSERT INTO `transactions` (`id`, `user_id`, `value`, `currencies_id`, `type`) VALUES
-(1, 6, 60, 1, 'depot');
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -136,16 +100,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `register_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `login_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `number_phone`, `password`, `status`, `register_date`, `login_date`) VALUES
-(6, 'Celian', 'Loisel', 'celianloisel@gmail.com', '0661804927', 'e4d3462aca45391b6da862a1fb62d37b661c4a6a4272acb3e91fada40d0b7695', 200, '2023-02-02 10:21:00', '2023-02-02 10:21:00'),
-(8, 'test2', 'test2', 'test@test.com2', '00000000002', 'e4d3462aca45391b6da862a1fb62d37b661c4a6a4272acb3e91fada40d0b7695', 10, '2023-02-02 16:15:53', '2023-02-02 16:15:53'),
-(7, 'test', 'test', 'test@test.com', '0000000000', 'e4d3462aca45391b6da862a1fb62d37b661c4a6a4272acb3e91fada40d0b7695', 10, '2023-02-02 16:15:53', '2023-02-02 16:15:53');
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -155,10 +110,11 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `number_phone`, `pa
 
 DROP TABLE IF EXISTS `withdrawals`;
 CREATE TABLE IF NOT EXISTS `withdrawals` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `value` float NOT NULL,
-  `currencies_id` int(11) NOT NULL
+  `currencies_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
